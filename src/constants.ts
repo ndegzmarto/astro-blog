4aa3f9e47e64cf07852e327229341e1731eb29fd
ndegz <ndegzmarto@gmail.com>
2024-02-15 19:28:44 +0300

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Tech Blog";
+export const SITE_DESCRIPTION = "Articles, stories & tutorials for tech people";
+export const HOME_PAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
