691daf4c13f621b149e8a40f6dc1e0321bec1026
ndegz <ndegzmarto@gmail.com>
2024-02-20 02:41:42 +0300

    edited tags

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+// format date to a string
 function formatDate(date: Date): string {
     const options: Intl.DateTimeFormatOptions = {
       year: 'numeric',
@@ -7,4 +8,14 @@ function formatDate(date: Date): string {
     return new Date(date).toLocaleDateString(undefined, options);
 }
 
-export { formatDate };
+
+
+// capitalize
+function capitalize(str: string): string{
+  if(typeof str != 'string' || str.length === 0){
+    return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export { formatDate, capitalize };
